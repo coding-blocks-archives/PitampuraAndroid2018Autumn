5d5f38ad2001f49f8a9006a3fe2e917c40dc5e12
the-dagger <harshithdwivedi@gmail.com>
2018-09-09 14:14:01 +0530

    add code for fragments
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

---
+++ Lecture06/Fragments/app/src/main/java/com/codingblocks/fragments/MainActivityFragment.java
@@ -0,0 +1,4 @@
+package com.codingblocks.fragments;
+
+public class MainActivityFragment {
+}
---
+++ Lecture06/Fragments/app/src/main/java/com/codingblocks/fragments/MainActivityFragment2.java
@@ -0,0 +1,34 @@
+package com.codingblocks.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivityFragment extends Fragment{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+
+        return view;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //All the findViewById will happen here
+    }
+}
