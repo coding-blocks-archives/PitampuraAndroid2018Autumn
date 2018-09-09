f2c57f950fba882802d9aca973bc818b28835143
the-dagger <harshithdwivedi@gmail.com>
2018-09-09 15:45:37 +0530

    add code for master-detail
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

---
+++ Lecture06/RecyclerViewFragment/app/src/main/java/com/codingblocks/recyclerviewfragment/MainActivityFragment.java
@@ -0,0 +1,57 @@
+package com.codingblocks.recyclerviewfragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class MainActivityFragment extends Fragment {
+
+    ArrayList<SuperHero> superHeroes = new ArrayList<>();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        superHeroes.add(new SuperHero("Batman", "DC", "Rich"));
+        superHeroes.add(new SuperHero("Superman", "DC", "Superhuman abilities"));
+        superHeroes.add(new SuperHero("Captain America", "Marvel", "American"));
+        superHeroes.add(new SuperHero("Ironman", "Marvel", "Rich"));
+        superHeroes.add(new SuperHero("Green Lantern", "DC", "Magic Ring"));
+        superHeroes.add(new SuperHero("Black Widow", "Marvel", "Agent"));
+        superHeroes.add(new SuperHero("Shazam", "DC", "God's man"));
+        superHeroes.add(new SuperHero("Hulk", "Marvel", "Smash"));
+        superHeroes.add(new SuperHero("Batman", "DC", "Rich"));
+        superHeroes.add(new SuperHero("Superman", "DC", "Superhuman abilities"));
+        superHeroes.add(new SuperHero("Captain America", "Marvel", "American"));
+        superHeroes.add(new SuperHero("Ironman", "Marvel", "Rich"));
+        superHeroes.add(new SuperHero("Green Lantern", "DC", "Magic Ring"));
+        superHeroes.add(new SuperHero("Black Widow", "Marvel", "Agent"));
+        superHeroes.add(new SuperHero("Shazam", "DC", "God's man"));
+        superHeroes.add(new SuperHero("Hulk", "Marvel", "Smash"));
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        SuperheroAdapter superheroAdapter = new SuperheroAdapter(superHeroes, (AppCompatActivity) getActivity());
+
+        recyclerView.setAdapter(superheroAdapter);
+    }
+}
